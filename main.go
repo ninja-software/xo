d9d18e1e533c9570feb2c0c7aae9eb1c828d8ba9
John Nguyen <jtnguyen236@gmail.com>
2021-02-07 09:00:29 +0800

    add error output from exec

--- main.go
+++ main.go
@@ -358,7 +358,7 @@ func writeTypes(args *internal.ArgType) error {
 	// process written files with goimports
 	output, err := exec.Command("goimports", params...).CombinedOutput()
 	if err != nil {
-		return errors.New(string(output))
+		return errors.New(err.Error() + ":" + string(output))
 	}
 
 	return nil
